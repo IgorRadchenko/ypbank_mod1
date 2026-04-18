e52296914c49610f6b805ed49d60dc0e94ef94e9
IgorRadchenko <igo46955774@yandex.ru>
2026-04-18 13:10:54 +0300

    converter: added parsing args impl

--- ypbank/src/bin/ypbank_converter.rs
+++ ypbank/src/bin/ypbank_converter.rs
@@ -6,8 +6,39 @@ ypbank_converter \
   --input-format <format> \
   --output-format <format> \
   > output_file.txt
- */
+*/
+
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let mut input_file: String = "".to_string();
+    let mut input_format: String = "".to_string();
+    let mut output_format: String = "".to_string();
+
+    for i in 1..args.len() {
+        //println!("Got: {i}");
+
+        match args[i].as_str() {
+            "--input" => {
+                input_file = args[i+1].clone();
+            }
+            "--input_format" => {
+                input_format = args[i+1].clone();
+            }
+            "--output-format" => {
+                output_format = args[i+1].clone();
+            }
+            _ => {
+                //eprintln!("Неизвестная команда {i}");
+            }
+        }
+    }
+
+    println!("input_file: '{input_file}'");
+    println!("input_format: '{input_format}'");
+    println!("output_format: '{output_format}'");
 
 }
